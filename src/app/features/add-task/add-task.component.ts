676d56fc32096689e7465a7ad14d1677c6ec766a
Tobias Reize <reizeto@web.de>
2025-05-22 15:30:11 +0200

    added priorities to AddTaskComponent

--- src/app/features/add-task/add-task.component.ts
+++ src/app/features/add-task/add-task.component.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { SidenavComponent } from "../../shared/sidenav/sidenav.component";
 import { HeaderComponent } from "../../shared/header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [SidenavComponent, HeaderComponent],
+  imports: [CommonModule, SidenavComponent, HeaderComponent],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.scss'
 })
 export class AddTaskComponent {
+
+  selectedPriority: string = 'medium';
+
+
+  choosePriority(priority: string) {
+    this.selectedPriority = priority;
+  }
 
 }
