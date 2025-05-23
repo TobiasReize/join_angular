c7a5b33a64a42391f80874e43405663517b8f949
Tobias Reize <reizeto@web.de>
2025-05-23 10:46:59 +0200

    added contact dropdown to AddTaskComponent and some updates

--- src/app/features/add-task/add-task.component.ts
+++ src/app/features/add-task/add-task.component.ts
@@ -13,10 +13,49 @@ import { CommonModule } from '@angular/common';
 export class AddTaskComponent {
 
   selectedPriority: string = 'medium';
+  contactsVisible: boolean = false;
+  testContacts: string[] = [
+    'Sophie Müller',
+    'Hans Lustig',
+    'Max Mustermann',
+    'Erika Musterfrau',
+    'Gast',
+    'Tim Gross',
+    'Eva Klein'
+  ];
+  selectedContacts: string[] = [];
 
 
   choosePriority(priority: string) {
     this.selectedPriority = priority;
+  }
+
+
+  toggleContactsOverlay(event?: Event, state?: 'visible') {
+    if (state == 'visible') {
+      this.contactsVisible = true;
+    } else {
+      this.contactsVisible = !this.contactsVisible;
+      event?.stopPropagation();
+    }
+  }
+
+
+  selectContact(contact: string) {
+    const index = this.selectedContacts.indexOf(contact);
+    
+    if (index == -1) {
+      this.selectedContacts.push(contact);
+    } else {
+      this.selectedContacts.splice(index, 1);
+    }
+  }
+
+
+  getInitials(name: string) {
+    const words = name.split(' ');
+    const initials = words.map(word => word.charAt(0).toUpperCase()).join('');
+    return initials;
   }
 
 }
