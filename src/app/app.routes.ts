14571c684da5fd5d9baaaea7c19c8dbc0060ee64
Tobias Reize <reizeto@web.de>
2025-05-30 14:22:22 +0200

    added BoardComponent and updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './features/login/login.component';
 import { SignupComponent } from './features/signup/signup.component';
 import { SummaryComponent } from './features/summary/summary.component';
 import { AddTaskComponent } from './features/add-task/add-task.component';
+import { BoardComponent } from './features/board/board.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'summary', component: SummaryComponent },
     { path: 'addtask', component: AddTaskComponent },
+    { path: 'board', component: BoardComponent },
 ];
