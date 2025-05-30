4b886636d34866615a2556d4dda7b15557324b7a
Tobias Reize <reizeto@web.de>
2025-05-30 14:14:57 +0200

    added mobile-greeting-overlay to SummaryComponent

--- src/app/features/summary/summary.component.ts
+++ src/app/features/summary/summary.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SidenavComponent } from "../../shared/sidenav/sidenav.component";
 import { HeaderComponent } from "../../shared/header/header.component";
 
@@ -9,6 +9,15 @@ import { HeaderComponent } from "../../shared/header/header.component";
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnInit {
+
+  greetingVisible: boolean = true;
+
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.greetingVisible = false;
+    }, 1600);
+  }
 
 }
