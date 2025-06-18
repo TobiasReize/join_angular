d3d36d7e8f6d7aa1951b0bc23eef3bf3194b0464
Tobias Reize <reizeto@web.de>
2025-06-18 15:19:49 +0200

    added drag and drop to BoardComponent

--- src/app/features/board/board.component.ts
+++ src/app/features/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidenavComponent } from '../../shared/sidenav/sidenav.component';
 import { HeaderComponent } from '../../shared/header/header.component';
@@ -7,7 +7,6 @@ import { Task } from '../../models/task.class';
 import { TaskService } from '../../services/task-service/task.service';
 import { CardDetailViewComponent } from './card-detail-view/card-detail-view.component';
 import { FirebaseService } from '../../services/firebase-service/firebase.service';
-import { ContactService } from '../../services/contact-service/contact.service';
 import { UserService } from '../../services/user-service/user.service';
 
 @Component({
@@ -20,10 +19,11 @@ import { UserService } from '../../services/user-service/user.service';
 export class BoardComponent implements OnInit {
 
   taskService = inject(TaskService);
-  contactService = inject(ContactService);
-  firebaseService = inject(FirebaseService);
-  userService = inject(UserService);
+  private firebaseService = inject(FirebaseService);
+  private userService = inject(UserService);
   filteredTasks = this.taskService.allTasks;
+  private currentDraggedTaskID = signal<string>('');
+  dragOver = signal<string>('');
 
 
   ngOnInit(): void {
@@ -43,6 +43,29 @@ export class BoardComponent implements OnInit {
     } else {
       this.filteredTasks = this.taskService.allTasks;
     }
+  }
+
+
+  startDragging(taskId: string) {
+    this.currentDraggedTaskID.set(taskId);
+  }
+
+
+  allowDrop(event: DragEvent, column: string) {
+    event.preventDefault();
+    this.dragOver.set(column);
+  }
+
+
+  removeHighlight() {
+    this.dragOver.set('');
+  }
+
+
+  moveTo(column: string) {
+    const data = {column: column};
+    this.dragOver.set('');
+    this.firebaseService.updateDocData('tasks', this.currentDraggedTaskID(), data);
   }
 
 
