1379c536a83a2f51a494cce2d07eea4965956e4d
Tobias Reize <reizeto@web.de>
2025-06-24 09:54:26 +0200

    added mobile swap functions to CardComponent

--- src/app/features/board/card/card.component.ts
+++ src/app/features/board/card/card.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, HostListener, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TaskInterface } from '../../../interfaces/task.interface';
+import { Task } from '../../../models/task.class';
 import { Contact } from '../../../models/contact.class';
 import { ContactService } from '../../../services/contact-service/contact.service';
+import { FirebaseService } from '../../../services/firebase-service/firebase.service';
 
 @Component({
   selector: 'app-card',
@@ -13,12 +14,55 @@ import { ContactService } from '../../../services/contact-service/contact.servic
 })
 export class CardComponent {
 
-  @Input() task!: TaskInterface;
-  @Input() column!: 'To do' | 'In progress' | 'Await feedback' | 'Done';
   contactService = inject(ContactService);
+  firebaseService = inject(FirebaseService);
+  @Input() task!: Task;
+  @Input() column!: 'To do' | 'In progress' | 'Await feedback' | 'Done';
+  columns = ['To do', 'In progress', 'Await feedback', 'Done'];
+  swapOverlayVisible: boolean = false;
+  previous: string = '';
+  next: string = '';
 
 
-  getSubtasksDone(task: TaskInterface): number {
+  @HostListener('document:click', ['$event'])
+  onDocumentClick() {
+    this.swapOverlayVisible = false;
+  }
+
+
+  showSwapOverlay(event: Event) {
+    event.stopPropagation();
+    this.swapOverlayVisible = true;
+    const index = this.columns.indexOf(this.task.column);
+    this.setColumns(index);
+  }
+
+
+  setColumns(index: number) {
+    if (index - 1 < 0) {
+      this.previous = '-';
+      this.next = 'In progress';
+    } else if (index + 1 > 3) {
+      this.next = '-';
+      this.previous = 'Await feedback';
+    } else {
+      this.previous = this.columns[index - 1];
+      this.next = this.columns[index + 1];
+    }
+  }
+
+
+  moveTo(column: string) {
+    if (column != '-') {
+      const data = {column: column};
+      this.firebaseService.updateDocData('tasks', this.task.id, data);
+    }
+    this.swapOverlayVisible = false;
+  }
+
+
+  // Hilfsfunktionen:
+  getSubtasksDone(task: Task): number {
     return task.subtasks.filter(subtask => subtask.status === 'done').length;
   }
 
