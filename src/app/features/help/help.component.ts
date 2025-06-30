3d4f45cc881c8bf2a626cac0791a21cae4ea533c
Tobias Reize <reizeto@web.de>
2025-06-30 10:06:48 +0200

    updated HelpComponent

--- src/app/features/help/help.component.ts
+++ src/app/features/help/help.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { SidenavComponent } from '../../shared/sidenav/sidenav.component';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { UserService } from '../../services/user-service/user.service';
 
 @Component({
   selector: 'app-help',
   standalone: true,
-  imports: [],
+  imports: [SidenavComponent, HeaderComponent],
   templateUrl: './help.component.html',
   styleUrl: './help.component.scss'
 })
-export class HelpComponent {
+export class HelpComponent implements OnInit {
+
+  private location = inject(Location);
+  private userService = inject(UserService);
+
+
+  ngOnInit(): void {
+    this.userService.checkCredentials();
+  }
+
+
+  goBack() {
+    this.location.back();
+  }
 
 }
